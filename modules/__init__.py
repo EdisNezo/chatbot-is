fd0e615fc439686601ed17634daa5503caf6049f
EdisNezo <edis.nezovski@outlook.de>
2025-03-20 21:52:34 +0100

    Version 1.1

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,9 +1,3 @@
-from modules.document_processor import DocumentProcessor
-from modules.vector_store_manager import VectorStoreManager
-from modules.llm_manager import LLMManager
-from modules.template_manager import TemplateManager
-from modules.dialog_manager import DialogManager
-from modules.elearning_generator import ELearningCourseGenerator
 
 __all__ = [
     'DocumentProcessor',
